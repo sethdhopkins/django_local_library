69de1782eacb85f0161aefc89a229b59a63bf5e4
sethdhopkins <sethdhopkins2020@gmail.com>
2026-06-19 15:25:12 -0500

    Added url mapping for index page and view templates.

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
 def index(request):
-    return render(request, 'catalog/index.html')
+    """View function for home page of site."""
+
+    #Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    #Available books
+    num_bookInstances_available = BookInstance.objects.filter(status__exact = 'a').count()
+
+    #The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_bookInstances_available': num_bookInstances_available,
+        'num_authors': num_authors,
+    }
+
+    #Renders the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
